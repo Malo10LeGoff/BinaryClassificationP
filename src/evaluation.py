8bad9943c673e5d474a0f9c0d8d6496ca5c1410e
Minh Tri <minhtri3007@hotmail.com>
2021-11-26 20:30:02 +0100

    [UPD] - Code refactoring. Changed some function signatures for code readability

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,10 +1,22 @@
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
-def evaluate_model(*, y_pred, y_test):
+def evaluate_model(model, x_test, y_true, metric=accuracy_score):
+    """
+    Evaluate a model's classification against a given metric.
+    By default evaluates with by accuracy score.
+    :param y_pred: The output from the model's predictions.
+    :param y_true: Ground truth
+    :param metric: The metric to evaluate against. Takes y_pred and y_true and outputs a score
+    :return: the result of the evaluation
+    """
+    # get the model's predictions
+    y_pred = model.predict(x_test)
 
-    conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    return metric(y_true, y_pred)
 
-    print(f"confusion matrix : ")
-    print(conf_matrix)
-    print(f"Accuracy : {accuracy_score(y_test, y_pred)}")
+
+def conf_matrix(model, x_test, y_true):
+    # calculate the model's confusion matrix
+    y_pred = model.predict(x_test)
+    return confusion_matrix(y_true, y_pred)
--- src/main.py
+++ src/main.py
@@ -1,53 +1,45 @@
 from numpy.random.mtrand import random
 import pandas as pd
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from evaluation import evaluate_model
-from preprocessing import normalize_dataset
+from evaluation import evaluate_model, conf_matrix
+from preprocessing import normalize_dataset, preprocess
 from utility_functions import get_num_cat_features
 from preprocessing import fill_na_values, normalize_dataset, feature_selection
 
+if __name__ == '__main__':
+    ### Import the dataset
+    df = pd.read_csv("../data/kidney_disease.csv", sep=",")
 
-### Import the dataset
-df = pd.read_csv("../data/kidney_disease.csv", sep=",")
+    ### Clean the missing values
+    category_columns, numerical_columns = get_num_cat_features(df=df)
 
+    print(type(category_columns))
 
-### Clean the missing values
-category_columns, numerical_columns = get_num_cat_features(df=df)
+    df = fill_na_values(
+        df=df, category_columns=category_columns, numerical_columns=numerical_columns
+    )
 
-print(type(category_columns))
+    y = df["classification"]
+    x = df.drop(columns=["classification"])
 
-df = fill_na_values(
-    df=df, category_columns=category_columns, numerical_columns=numerical_columns
-)
+    ### Split the dataset
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.33, random_state=42
+    )
 
-y = df["classification"]
-X = df.drop(columns=["classification"])
+    # Preprocess the split
+    x_train_preprocessed = preprocess(data=x_train, numerical_columns=numerical_columns)
+    x_test_preprocessed = preprocess(data=x_test, numerical_columns=numerical_columns)
 
+    ### Training
+    model = RandomForestClassifier(random_state=42)
+    model.fit(x_train_preprocessed, y_train)
 
-### Split the dataset
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42
-)
-
-
-### Normalize the dataset
-X_train_scaled, X_test_scaled = normalize_dataset(
-    X_train=X_train, X_test=X_test, numerical_columns=numerical_columns
-)
-
-
-### Feature selection (PCA). PCA must be performed before normalization
-X_train_scaled_reduced, X_test_scaled_reduced = feature_selection(
-    X_train=X_train_scaled, X_test=X_test_scaled
-)
-
-
-### Training
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train_scaled_reduced, y_train)
-
-
-### Predict and Evaluate
-y_pred = model.predict(X_test_scaled_reduced)
-evaluate_model(y_pred=y_pred, y_test=y_test)
+    ### Predict and Evaluate
+    result = evaluate_model(model=model, x_test=x_test_preprocessed, y_true=y_test, metric=accuracy_score)
+    confusion_matrix = conf_matrix(model=model, x_test=x_test_preprocessed, y_true=y_test)
+    print(f"confusion matrix : ")
+    print(confusion_matrix)
+    print(f"Accuracy : {result}")
